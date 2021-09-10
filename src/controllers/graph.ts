4e0541486282e11cb0fc9c9d2c098018f5fbfe01
unknown <samuel871211@gmail.com>
2021-09-10 10:28:51 +0800

    try catch in async await is required, or it will cause unhandled promise rejection error

--- src/controllers/graph.ts
+++ src/controllers/graph.ts
@@ -1,30 +1,37 @@
 import { MongoClient } from 'mongodb'
 import { Request, Response } from 'express'
 import dotenv from 'dotenv'
-// const MongoClient = require('mongodb').MongoClient
-// const express = require('express')
-// const dotenv = require('dotenv')
 dotenv.config()
 const mongoUrl = process.env.MONGO_URL || ''
 const dbName = process.env.MONGO_DBNAME || ''
 
 class Graph {
 	async get (req: Request, res: Response) {
-		const db = await MongoClient.connect(mongoUrl)
-		const data = await db.db(dbName).collection('graph').findOne(
-			{ title: 'world' },
-			{ projection: { _id: 0, title: 0 } }
-		)
-		res.send(data)
+		try {
+			const db = await MongoClient.connect(mongoUrl)
+			const data = await db.db(dbName).collection('graph').findOne(
+				{ title: 'world' },
+				{ projection: { _id: 0, title: 0 } }
+			)
+			res.send(data)
+		} catch (e) {
+			console.log(e)
+			res.status(500).send({ cells: [] })
+		}
 	}
 
 	async put (req: Request, res: Response) {
-		const db = await MongoClient.connect(mongoUrl)
-		await db.db(dbName).collection('graph').updateOne(
-			{ title: 'world' },
-			{ $set: req.body }
-		)
-		res.send(req.body)
+		try {
+			const db = await MongoClient.connect(mongoUrl)
+			await db.db(dbName).collection('graph').updateOne(
+				{ title: 'world' },
+				{ $set: req.body }
+			)
+			res.send(req.body)
+		} catch (e) {
+			console.log(e)
+			res.status(500).send()
+		}
 	}
 }
 
