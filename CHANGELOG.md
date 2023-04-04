0417b14443134d5b18d71ccae9bfcd1cdf911734
unknown <samuel871211@gmail.com>
2023-04-04 18:11:47 +0800

    fix: root dir bug fix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 1.0.1
+
+### fixed
+
+- root dir bug fix
+
 ## 1.0.0
 
 ### added
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 const app = express();
 app.use(
   "/imgs",
-  express.static(`${__dirname}/src/static/imgs`, { maxAge: 31536000 })
+  express.static(`${process.cwd()}/src/static/imgs`, { maxAge: 31536000 })
 );
 app.get("/", (req: Request, res: Response) =>
   res.send("welcome to dokapon API Service")
